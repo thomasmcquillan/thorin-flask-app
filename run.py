a232062f729211219368853d2a448407b01b06c7
thomasmcquillan <thomas.mcquillan@gmail.com>
2022-01-18 14:44:52 +0000

    Add member html template to display additional character information using advanced routing

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-   
 import os
 import json
 from flask import Flask, render_template
@@ -18,6 +17,17 @@ def about():
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
+
+
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
 
 
 @app.route("/contact")
