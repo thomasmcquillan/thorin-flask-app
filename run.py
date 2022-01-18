861e8732d15ddcb78bcedbc8b4c57b382a44eb7a
thomasmcquillan <thomas.mcquillan@gmail.com>
2022-01-18 15:57:29 +0000

    Add form to contact page and update contact route and function in run.py to enable post method

--- run.py
+++ run.py
@@ -30,8 +30,11 @@ def about_member(member_name):
     return render_template("member.html", member=member)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        print(request.form.get("name"))
+        print(request.form["email"])
     return render_template("contact.html", page_title="Contact")
 
 
